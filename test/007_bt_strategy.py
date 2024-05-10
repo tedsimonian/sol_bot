3b54347ddff887851071d77e128e8d1f1c30b71d
Ted Simonian <tedsimonian@gmail.com>
2024-05-10 15:34:38 -0700

    feat: removed unused strategies or incomplete ones. Started developing winning strategy

--- test/007_bt_strategy.py
+++ test/007_bt_strategy.py
@@ -2,62 +2,69 @@
 Strategy_007
 
 Technical Indicators: 
+    - Parabolic SAR
+    - Exponential Moving Average (EMA) 
     - Moving Average Convergence Divergence (MACD)
+    - Relative Strength Index (RSI)
 
-Strategy Description:
-    This strategy is based on the Moving Average Convergence Divergence (MACD) indicator to identify potential trading opportunities.
-    When the MACD line crosses above the signal line, the strategy enters a long position.
-    When the MACD line crosses below the signal line, the strategy enters a short position.
-    The strategy includes mechanisms to exit trades when the MACD line crosses below the signal line for long positions or above the signal line for short positions.
+Timeframe: 
+    - 2h
 
-Entry:
-    - MACD line crosses above the signal line, a long position is entered.
-    - MACD line crosses below the signal line, a short position is entered.
+Buy (Long) Entry:
+    - Enter a buy trade when the price is above the EMA, indicating an upward trend.
+    - Confirm the buy signal by observing the Parabolic SAR indicating an upward trend and the MACD Delta being positive.
 
-Exit:
-    - MACD line crosses below the signal line, a long position is exited.
-    - MACD line crosses above the signal line, a short position is exited.
+Buy (Long) Exit:
+    - Open a sell trade when the price is below the EMA, signaling a downward trend.
+    - Confirm the sell signal by noting the Parabolic SAR indicating a downward trend and the MACD Delta being negative.
+    
 """
 
 from backtesting import Backtest, Strategy
 
 from utilities import fetch_data_from_exchange
 from connector import get_kucoin_connection
-from constants import COMMISSION, LIMIT, STARTING_CAPITAL, SYMBOL, TIMEFRAME
+from constants import COMMISSION, LIMIT, STARTING_CAPITAL, SYMBOL
 
-import warnings
-import pandas_ta as ta
 import pandas as pd
+import pandas_ta as ta
+import warnings
 
 # filter all warnings
 warnings.filterwarnings('ignore')  # Setting the warnings to be ignored
 
+TIMEFRAME = '2h'
+
 class Strategy_007(Strategy):
-    macd_short_period = 12
-    macd_long_period = 26
+    ema_period = 200
+    rsi_period = 14
     
     def init(self):
         close_series = pd.Series(self.data.Close)
-        # Initialize the indicators
-        self.macd = self.I(ta.macd, close_series, self.macd_short_period, self.macd_long_period)
+        high_series = pd.Series(self.data.High)
+        low_series = pd.Series(self.data.Low)
+        
+        self.ema = self.I(ta.ema, close_series, self.ema_period)
+        self.macd = self.I(ta.macd, close_series)
+        self.psar = self.I(ta.psar, high_series, low_series, close_series)
+        self.rsi = self.I(ta.rsi, close_series, self.rsi_period)
 
-    def next(self):
-        # If the MACD line crosses above the signal line, and we're not already in a long position
-        if (self.macd[-1] > 0) and (not self.position.is_long):
+    def next(self):        
+        # If the price is above the EMA, 
+        # confirm long by observing the Parabolic SAR indicating an upward trend (psar == 1) 
+        # the MACD Delta being positive
+        # and the RSI being above 70
+        if self.data.Close[-1] > self.ema[-1] and self.psar[-1] == 1 and self.macd[-1] > 0 and self.rsi[-1] > 70:
             self.buy()
-        
-        # If the MACD line crosses below the signal line, and we're not already in a short position
-        if (self.macd[-1] < 0) and (not self.position.is_short):
-            self.sell()
-        
-        # Close long position when the MACD line crosses below the signal line
-        if self.position.is_long and (self.macd[-1] < 0):
-            self.position.close()
-        
-        # Close short position when the MACD line crosses above the signal line
-        if self.position.is_short and (self.macd[-1] > 0):
-            self.position.close()
 
+                
+        # If the price is below the EMA,
+        # confirm the sell signal by observing the Parabolic SAR indicating a downward trend (psar == 0)
+        # the MACD Delta being negative
+        # and the RSI being below 30
+        if self.data.Close[-1] < self.ema[-1] and self.psar[-1] == 0 and self.macd[-1] < 0 and self.rsi[-1] < 30:
+            self.position.close()
+        
 
 
 exchange = get_kucoin_connection()  # Establish connection to Kucoin
